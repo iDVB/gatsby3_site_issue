49fd3a73fc0c64c5f161f00b7475718c1e7cf7fe
iDVB <me@danvanbrunt.com>
2021-04-26 13:44:24 -0400

    WIP

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,14 +1,6 @@
 module.exports = {
   pathPrefix: '/',
   plugins: [
-    {
-      resolve: `gatsby-plugin-material-ui`,
-      options: {
-        stylesProvider: {
-          injectFirst: true,
-        },
-      },
-    },
     `gatsby-plugin-styled-components`,
   ],
   flags: {
